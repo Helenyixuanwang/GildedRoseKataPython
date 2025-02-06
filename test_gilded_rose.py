abbcf7a9c006ff9c56ba6055bfb1f87acb138b5e
Helen Wang <wangyixuan@msn.com>
2025-02-05 19:31:13 -0800

    Add two more logical tests - all tests confirmed failing as required

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -23,6 +23,24 @@ class GildedRoseTest(unittest.TestCase):
         all_items = gilded_rose.get_items()
         self.assertEquals(["Sulfuras"], all_items)
 
+    def test_backstage_passes_quality_changes(self):
+        """Test that Backstage passes increase in quality as concert approaches"""
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+    
+        # Quality should increase by 2 when there are 10 days or less
+        self.assertEquals(22, items[0].quality)
+        self.assertEquals(10, items[0].sell_in)
+
+    def test_item_quality_never_negative(self):
+        """Test that the quality of an item is never negative"""
+        items = [Item("regular item", 5, 0)]  # Start with quality 0
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+    
+        self.assertGreaterEqual(items[0].quality, 0)
+        self.assertEquals(4, items[0].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
